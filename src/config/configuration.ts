ba5fceabe38f4d8812046f565b0ac3c0c800af79
aseleznev <aa.seleznev@gmail.com>
2020-10-12 16:07:54 +0500

    add default values

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,10 @@
 export default () => ({
   port: parseInt(process.env.NODE_PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST,
+    host: process.env.DATABASE_HOST || 'localhost',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME
+    user: process.env.DATABASE_USER || 'test',
+    password: process.env.DATABASE_PASSWORD || 'test',
+    name: process.env.DATABASE_NAME || 'bell'
   }
 });
